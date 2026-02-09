ccee55004e083ff29a1081f1cdef1f7acded8660
杨再涛 <1772081518@qq.com>
2026-02-10 00:00:26 +0800

    初始化

---
+++ src/main/java/com/hkhj4/Hkhj4Application.java
@@ -0,0 +1,13 @@
+package com.hkhj4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hkhj4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hkhj4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hkhj4/Hkhj4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkhj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hkhj4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
